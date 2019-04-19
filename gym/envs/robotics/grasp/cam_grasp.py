eeb41361f4c45e1532b77a3ead577a27a7a911db
Zisu Dong <jdong1021@fb.com>
2019-04-19 17:00:52 -0400

    grasp env

---
+++ gym/envs/robotics/grasp/cam_grasp.py
@@ -0,0 +1,25 @@
+import os
+from gym import utils
+from gym.envs.robotics import grasp_env
+
+
+# Ensure we get the path separator correct on windows
+MODEL_XML_PATH = os.path.join('fetch', 'pick_and_place.xml')
+
+
+class CamGraspEnv(grasp_env.GraspEnv, utils.EzPickle):
+    def __init__(self, reward_type='dense', goal_type='fixed', cam_type='fixed', gripper_init_type='fixed', act_noise=False, obs_noise=False, open_loop=True):
+        initial_qpos = {
+            'robot0:slide0': 0.405,
+            'robot0:slide1': 0.48,
+            'robot0:slide2': 0.0,
+            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+        }
+        grasp_env.GraspEnv.__init__(
+            self, MODEL_XML_PATH, has_object=True, block_gripper=False, n_substeps=20,
+            gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
+            obj_range=0.15, target_range=0.15, distance_threshold=0.05,
+            initial_qpos=initial_qpos, reward_type=reward_type, goal_type=goal_type,
+            cam_type=cam_type, gripper_init_type=gripper_init_type, act_noise=act_noise, obs_noise=obs_noise,
+            joint_training=False, open_loop=open_loop)
+        utils.EzPickle.__init__(self)
