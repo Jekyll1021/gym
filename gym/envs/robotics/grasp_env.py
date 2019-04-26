b5f67713231d1270736608040b5c701acbc04329
Zisu Dong <jdong1021@fb.com>
2019-04-26 15:54:40 -0400

    reward signal

--- gym/envs/robotics/grasp_env.py
+++ gym/envs/robotics/grasp_env.py
@@ -284,6 +284,7 @@ class GraspEnv(robot_env.RobotEnv):
     def _sample_goal(self):
         goal = self.sim.data.get_site_xpos('object0').copy()
         goal[2] += self.distance_threshold
+        goal[2] -= 0.01
 
         return goal.copy()# - self.sim.data.get_site_xpos("robot0:cam")
 
