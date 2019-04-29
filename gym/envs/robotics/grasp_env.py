53911c7b6d251316da95bcf29cf6492b7ba89084
Zisu Dong <jdong1021@fb.com>
2019-04-29 16:11:04 -0400

    no other input except counter

--- gym/envs/robotics/grasp_env.py
+++ gym/envs/robotics/grasp_env.py
@@ -240,10 +240,11 @@ class GraspEnv(robot_env.RobotEnv):
         counter = np.array([self.counter])
 
         achieved_goal = np.squeeze(object_pos.copy())# - self.sim.data.get_site_xpos("robot0:cam")
-        obs = np.concatenate([
-            grip_pos, object_pos.ravel(), object_rel_pos.ravel(), gripper_state, object_rot.ravel(),
-            object_velp.ravel(), object_velr.ravel(), grip_velp, gripper_vel, counter
-        ])
+        # obs = np.concatenate([
+        #     grip_pos, object_pos.ravel(), object_rel_pos.ravel(), gripper_state, object_rot.ravel(),
+        #     object_velp.ravel(), object_velr.ravel(), grip_velp, gripper_vel, counter
+        # ])
+        obs = counter.copy()
 
         return {
             'observation': obs.copy(),
