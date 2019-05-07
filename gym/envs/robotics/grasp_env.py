c5448c39b0ffbe71a9f4cf2230168a0ecfcef820
Zisu Dong <jdong1021@fb.com>
2019-05-07 16:21:49 -0400

    two cams

--- gym/envs/robotics/grasp_env.py
+++ gym/envs/robotics/grasp_env.py
@@ -231,9 +231,14 @@ class GraspEnv(robot_env.RobotEnv):
             img = self.sim.render(width=224, height=224, camera_name="external_camera_1", depth=True)[1]
         else:
             # img = self.sim.render(width=224, height=224, camera_name="external_camera_1") / 255
+            img = self.sim.render(width=224, height=224, camera_name="external_camera_2")
             # normalize by imagenet parameters
-            # img = (img - np.array([0.485, 0.456, 0.406]))/np.array([0.229, 0.224, 0.225])
-            img = self.sim.render(width=224, height=224, camera_name="external_camera_3")
+            img = (img - np.array([0.485, 0.456, 0.406]))/np.array([0.229, 0.224, 0.225])
+            # second image
+            img2 = self.sim.render(width=224, height=224, camera_name="external_camera_3")
+            # normalize by imagenet parameters
+            img2 = (img2 - np.array([0.485, 0.456, 0.406]))/np.array([0.229, 0.224, 0.225])
+            img = np.concatenate([img, img2], axis=-1)
         # positions
         grip_pos = self.sim.data.get_site_xpos('robot0:grip')
         holder_pos = grip_pos.copy()
