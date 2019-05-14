c4e5b9098a92c6423f8a9cf61e14028ec0b2a4ca
Zisu Dong <jdong1021@fb.com>
2019-05-14 15:47:54 -0400

    2 step push env

--- gym/envs/robotics/push/two_step_push.py
+++ gym/envs/robotics/push/two_step_push.py
@@ -16,7 +16,7 @@ class TwoStepPushEnv(push_env.PushEnv, utils.EzPickle):
             'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
         }
         push_env.PushEnv.__init__(
-            self, MODEL_XML_PATH, has_object=True, block_gripper=True, n_substeps=20,
+            self, MODEL_XML_PATH, block_gripper=True, n_substeps=20,
             gripper_extra_height=0.0, target_in_the_air=False, target_offset=0.0,
             obj_range=0.03, target_range=0.15, distance_threshold=0.02,
             initial_qpos=initial_qpos, reward_type=reward_type, goal_type=goal_type,
