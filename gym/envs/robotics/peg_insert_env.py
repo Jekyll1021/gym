17e4b71a09991705a8d76328552cdee7ecf79b87
Zisu Dong <jdong1021@fb.com>
2019-05-17 18:58:41 -0400

    resolution

--- gym/envs/robotics/peg_insert_env.py
+++ gym/envs/robotics/peg_insert_env.py
@@ -159,7 +159,7 @@ class PegInsertEnv(robot_env.RobotEnv):
                 img2 = (img2 - np.array([0.485, 0.456, 0.406]))/np.array([0.229, 0.224, 0.225])
                 img = np.concatenate([img, img2], axis=-1)
             else:
-                img = self.sim.render(width=500, height=500, camera_name="external_camera_1") / 255
+                img = self.sim.render(width=224, height=224, camera_name="external_camera_1") / 255
                 # normalize by imagenet parameters
                 img = (img - np.array([0.485, 0.456, 0.406]))/np.array([0.229, 0.224, 0.225])
         # positions
