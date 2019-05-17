03d5bf5488560663397ae15e70f0f1fd4a7f8bb9
Zisu Dong <jdong1021@fb.com>
2019-05-17 17:56:54 -0400

    peg hole relative

--- gym/envs/robotics/peg_insert/peg_insert.py
+++ gym/envs/robotics/peg_insert/peg_insert.py
@@ -16,7 +16,7 @@ class CamPegInsertEnv(peg_insert_env.PegInsertEnv, utils.EzPickle):
             'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
         }
         peg_insert_env.PegInsertEnv.__init__(
-            self, MODEL_XML_PATH, block_gripper=True, n_substeps=20,
+            self, MODEL_XML_PATH, block_gripper=False, n_substeps=20,
             gripper_extra_height=0.0, target_in_the_air=False, target_offset=0.0,
             obj_range=0.03, target_range=0.15, distance_threshold=0.02,
             initial_qpos=initial_qpos, reward_type=reward_type, goal_type=goal_type,
