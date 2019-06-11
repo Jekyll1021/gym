f9779c38aff0ab90040631148813070063263bd1
Zisu Dong <jdong1021@fb.com>
2019-06-11 11:26:52 -0400

    check reward signal

--- gym/envs/robotics/door_open_env.py
+++ gym/envs/robotics/door_open_env.py
@@ -83,7 +83,7 @@ class DoorOpenEnv(robot_env.RobotEnv):
     # ----------------------------
 
     def compute_reward(self, achieved_goal, goal, info):
-        return (goal_distance(self.sim.data.get_site_xpos('handletip'), self.init_tip) > 0.01).astype(np.float32)
+        return (goal_distance(self.sim.data.get_site_xpos('handletip'), self.init_tip) > 0.03).astype(np.float32)
 
     # RobotEnv methods
     # ----------------------------
@@ -271,7 +271,7 @@ class DoorOpenEnv(robot_env.RobotEnv):
         return False
 
     def _is_success(self, achieved_goal, desired_goal):
-        return (goal_distance(self.sim.data.get_site_xpos('handletip'), self.init_tip) > 0.01).astype(np.float32)
+        return (goal_distance(self.sim.data.get_site_xpos('handletip'), self.init_tip) > 0.03).astype(np.float32)
 
     def _env_setup(self, initial_qpos):
         for name, value in initial_qpos.items():
