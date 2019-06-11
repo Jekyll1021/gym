4f62f5433220a5521878f0d595e0ca5171a5f461
Zisu Dong <jdong1021@fb.com>
2019-06-11 13:00:18 -0400

    friction

--- gym/envs/robotics/door_open_env.py
+++ gym/envs/robotics/door_open_env.py
@@ -114,6 +114,7 @@ class DoorOpenEnv(robot_env.RobotEnv):
         utils.mocap_set_action(self.sim, action)
 
         if self.counter >= 2:
+            print(self.init_tip)
             self.sim.step()
             pos_ctrl = np.array([0.0, 0.0, 0.0])
             gripper_ctrl = np.array([-1, -1])
@@ -126,7 +127,7 @@ class DoorOpenEnv(robot_env.RobotEnv):
             # offset = obs_pos - grip_pos
             # print(offset, np.linalg.norm(offset, axis = -1))
 
-            for _ in range(10):
+            for _ in range(20):
                 utils.ctrl_set_action(self.sim, action)
                 self.sim.step()
 
@@ -134,14 +135,12 @@ class DoorOpenEnv(robot_env.RobotEnv):
             rot_init = np.array([0.7071068, 0.0, 0.7071068, 0])
             rot_target = np.array([0.5, 0.5, 0.5, -0.5])
             gripper_ctrl = np.array([-1, -1])
-
-            action = np.concatenate([pos_ctrl, rot_target, gripper_ctrl])
-            utils.mocap_set_action_abs(self.sim, action)
-            # for i in range(20):
-            #     rot_ctrl = rot_init + (rot_target - rot_init) * (i+1) / 20
-            #     action = np.concatenate([pos_ctrl, rot_ctrl, gripper_ctrl])
-            #     utils.mocap_set_action_abs(self.sim, action)
-            #     self.sim.step()
+            for i in range(20):
+                rot_ctrl = rot_init + (rot_target - rot_init) * (i+1) / 20
+                action = np.concatenate([pos_ctrl, rot_ctrl, gripper_ctrl])
+                utils.mocap_set_action_abs(self.sim, action)
+                self.sim.step()
+                print(self.sim.data.get_site_xpos('handletip'))
 
     def _get_obs(self):
         # images
