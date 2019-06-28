b76afd167af816996e7cfc361f1c7a79e114b5a3
Zisu Dong <jdong1021@fb.com>
2019-06-28 09:15:15 -0400

    after motion

--- gym/envs/robotics/drawer_open_env.py
+++ gym/envs/robotics/drawer_open_env.py
@@ -286,7 +286,7 @@ class DrawerOpenEnv(robot_env.RobotEnv):
 
     def _sample_goal(self):
         goal = self.sim.data.get_site_xpos('switch').copy()
-        goal[1] += 0.1
+        goal[1] += 0.2
 
         return goal.copy()# - self.sim.data.get_site_xpos("robot0:cam")
 
