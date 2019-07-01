4731e3ae1758a1ccb7119d2f1a602a14d2e9cad8
Zisu Dong <jdong1021@fb.com>
2019-07-01 17:56:52 -0400

    inverse peg insert

--- gym/envs/robotics/inverse_peg_insert_env.py
+++ gym/envs/robotics/inverse_peg_insert_env.py
@@ -307,7 +307,7 @@ class InversePegInsertEnv(robot_env.RobotEnv):
 
         box_pose = self.sim.data.get_site_xpos('object0').copy()
         pos_ctrl = box_pose.copy()
-        pos_ctrl[2] = box_pose[2]+0.7
+        pos_ctrl[2] = box_pose[2]+0.2
 
         action = np.concatenate([pos_ctrl, rot_ctrl, gripper_ctrl])
 
@@ -315,7 +315,7 @@ class InversePegInsertEnv(robot_env.RobotEnv):
 
         box_pose = self.sim.data.get_site_xpos('object0').copy()
         pos_ctrl = box_pose.copy()
-        pos_ctrl[2] = box_pose[2] + 0.5
+        pos_ctrl[2] = box_pose[2] + 0.04
 
         action = np.concatenate([pos_ctrl, rot_ctrl, gripper_ctrl])
 
