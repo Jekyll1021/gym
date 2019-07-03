7fcbefb416bc4176c8af1fb224e043ccdc764d68
Zisu Dong <jdong1021@fb.com>
2019-07-03 16:03:26 -0400

    cam pure translation

--- gym/envs/robotics/slide_env.py
+++ gym/envs/robotics/slide_env.py
@@ -299,8 +299,8 @@ class SlideEnv(robot_env.RobotEnv):
 
         if self.cam_type != "fixed":
             delta_pos = self.np_random.uniform(-0.05, 0.05, size=3)
-            delta_rot = self.np_random.uniform(-0.1, 0.1, size=3)
-            # delta_rot = np.array([0,0,0])
+            # delta_rot = self.np_random.uniform(-0.1, 0.1, size=3)
+            delta_rot = np.array([0,0,0])
             utils.cam_init_pos(self.sim, delta_pos, delta_rot)
 
         self.sim.forward()
