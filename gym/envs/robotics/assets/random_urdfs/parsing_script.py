fb4e90c7df60d287225ac50d367f334db739670b
Zisu Dong <jdong1021@fb.com>
2019-07-18 11:04:12 -0400

    grasp multi-obj debug

--- gym/envs/robotics/assets/random_urdfs/parsing_script.py
+++ gym/envs/robotics/assets/random_urdfs/parsing_script.py
@@ -24,7 +24,7 @@ def generate_env(model_path, obj_index, out_path):
     # step 4: read template, change template and write to xml
     tree = ET.parse(os.path.join(out_path, "grasp_template.xml"))
     root = tree.getroot()
-    root[3][0].attrib["file"] = new_model_path
+    root[3][0].attrib["file"] = os.path.join("..", new_model_path)
     root[3][0].attrib["scale"] = str(scale) + ' ' + str(scale) + ' ' + str(scale)
     # root[3][0].attrib -- {'file': path, 'name': 'obj0', 'scale': scale}
     root[4][4].attrib["pos"] = str(half_length[0]) + ' ' + str(half_length[1]) + ' ' + str(half_length[2])
