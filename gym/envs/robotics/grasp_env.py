72477bba940bdebef3ce367c451f70d4accccf5b
Zisu Dong <jdong1021@fb.com>
2019-08-02 18:17:57 -0400

    dyn cam debug

--- gym/envs/robotics/grasp_env.py
+++ gym/envs/robotics/grasp_env.py
@@ -227,10 +227,10 @@ class GraspEnv(robot_env.RobotEnv):
                 counter, [1, 0, 0]
             ])
         else:
-            obs = counter
-            # obs = np.concatenate([
-            #     counter, self.cam_offset.copy()
-            # ])
+            # obs = counter
+            obs = np.concatenate([
+                counter, self.cam_offset.copy()
+            ])
             # obs = np.empty(0)
 
         return {
